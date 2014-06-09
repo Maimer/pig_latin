d003868f24da95cb925c116ef7d513101c5a716d
Maimer <nesnl23@yahoo.com>
2014-06-09 11:39:06 -0400

    All tests pass for all 3 cases of words

--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -1,2 +1,18 @@
+require 'pry'
+
 class PigLatin
+
+  def initialize(word)
+    @word = word.downcase
+  end
+
+  def translate
+    if @word[0] =~ /^[^aeiou]+/
+      match_word = /^[^aeiou]+/.match(@word)
+      @word[match_word[0].length..-1] + match_word[0] + 'ay'
+    elsif @word[0] =~ /^[aeiou]/
+      @word + 'way'
+    end
+  end
+
 end
--- spec/pig_latin_spec.rb
+++ spec/pig_latin_spec.rb
@@ -1,8 +1,25 @@
 require_relative '../lib/pig_latin'
 
 describe PigLatin do
-  it 'works' do
-    pl = PigLatin.new
-    expect(true).to eq(true)
+  it 'translates a word starting with a consonant' do
+    pl = PigLatin.new('something')
+    expect(pl.translate).to eq('omethingsay')
   end
+
+  it 'translates a word starting with a vowel' do
+    pl = PigLatin.new('egg')
+    expect(pl.translate).to eq('eggway')
+  end
+
+  it 'translates a word starting with a cluster of consonants' do
+    pl = PigLatin.new('glove')
+    expect(pl.translate).to eq('oveglay')
+  end
+
+  it 'translates a word starting with a cluster of consonants' do
+    pl = PigLatin.new('straight')
+    expect(pl.translate).to eq('aightstray')
+  end
+
+
 end
